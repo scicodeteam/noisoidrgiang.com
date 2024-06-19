7049d6d50814c808690e18c19c28a73439e0970a
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-06-19 14:25:01 +0700

    update header

--- Header/header_drg_1_0_0/js/header_drg_1_0_0.js
+++ Header/header_drg_1_0_0/js/header_drg_1_0_0.js
@@ -1,64 +1,54 @@
+const queryHeader_dhh_1_1_0 = async () => {
+    const response = await fetch('https://noisoidrgiang.com/wp-json/api/v1/menu/');
+    const data = await response.json();
+    const dataResult = [];
+    data.id_header_drg_1_0_0_sub1.forEach(item => {
+        const dataTitle = item.title.split('\r\n');
+        const title = {
+            title: dataTitle[0],
+            link: dataTitle[1] ? dataTitle[1] : '#',
+        };
+        const col = {
+            submenu: item.col1 ? item.col1 : null,
+        }
+        const dataItem = { ...title, ...col };
+        dataResult.push(dataItem);
+    });
+    return dataResult;
+};
+
 const elmsLv1 = document.querySelectorAll('.isToggleMenu');
 elmsLv1.forEach(elmLv1 => {
-    if(!elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu--1')){
-        elmLv1.parentNode.insertAdjacentHTML('beforeend', '<div class="header_drg_1_0_0__submenu header_drg_1_0_0__submenu--1"></div>');
+    if (!elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu')) {
+        elmLv1.parentNode.insertAdjacentHTML('beforeend', '<div class="header_drg_1_0_0__submenu"></div>');
     }
 });
-const hoverHeader_drg_1_0_0 = () => {
+const hoverHeader_drg_1_0_0 = async () => {
+    const data = await queryHeader_dhh_1_1_0();
     elmsLv1.forEach(elmLv1 => {
         elmLv1.addEventListener('mousemove', () => {
-            const html = `
-            <div class="header_drg_1_0_0__subitem">
-                <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--2">NỘI SOI TIÊU HÓA trên</a>
-                <div class="header_drg_1_0_0__submenu header_drg_1_0_0__submenu--2">
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                </div>
-            </div>
-            <div class="header_drg_1_0_0__subitem">
-                <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--2">NỘI SOI TIÊU HÓA trên</a>
-                <div class="header_drg_1_0_0__submenu header_drg_1_0_0__submenu--2">
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                </div>
-            </div>
-            `;
-            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu--1').innerHTML = html;
+            const titleCurrent = elmLv1.getAttribute('data-title');
+            const dataIndex = data.find(x => x.title === titleCurrent);
+            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu').innerHTML = dataIndex.submenu;
         });
         document.querySelector('.header_drg_1_0_0__bg').addEventListener('mousemove', () => {
-            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu--1').innerHTML = '';
+            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu').innerHTML = '';
         });
     });
 };
 
-const clickHeader_drg_1_0_0 = () => {
+const clickHeader_drg_1_0_0 = async () => {
+    const data = await queryHeader_dhh_1_1_0();
     elmsLv1.forEach(elmLv1 => {
         elmLv1.addEventListener('click', () => {
-            const html = `
-            <div class="header_drg_1_0_0__subitem">
-                <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--2">NỘI SOI TIÊU HÓA trên</a>
-                <div class="header_drg_1_0_0__submenu header_drg_1_0_0__submenu--2">
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                </div>
-            </div>
-            <div class="header_drg_1_0_0__subitem">
-                <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--2">NỘI SOI TIÊU HÓA trên</a>
-                <div class="header_drg_1_0_0__submenu header_drg_1_0_0__submenu--2">
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                    <a href="#" class="header_drg_1_0_0__link header_drg_1_0_0__link--3">NỘI SOI TIÊU HÓA trên</a>
-                </div>
-            </div>
-            `;
-            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu--1').innerHTML = html;
+            elmLv1.parentNode.querySelector('.header_drg_1_0_0__submenu').innerHTML = data[0].submenu;
+            elmLv1.parentNode.classList.toggle('active');
         });
     });
 }
 
 const header_dhh_1_1_0 = () => {
-    if(window.innerWidth > 1024){
+    if (window.innerWidth > 1024) {
         hoverHeader_drg_1_0_0();
     } else {
         document.querySelector('.header_drg_1_0_0__toggle').addEventListener('click', () => {
@@ -75,6 +65,5 @@ const header_dhh_1_1_0 = () => {
 
 header_dhh_1_1_0();
 window.addEventListener('resize', () => header_dhh_1_1_0());
-
 
 
